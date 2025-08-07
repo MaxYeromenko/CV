f1ce1fd41b1593600eada2e938f6e55ea10b830c
MaxYeromenko <eromenko.max@gmail.com>
2025-08-07 16:09:27 +0300

    src added

--- js/set-current-date.js
+++
@@ -1,4 +0,0 @@
-const yearSpan = document.getElementById("current-year");
-const currentYear = new Date().getFullYear();
-
-yearSpan.textContent = currentYear;
--- js/to-top-visibility.js
+++
@@ -1,9 +0,0 @@
-const toTopButton = document.getElementById("to-top");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
-        toTopButton.style.display = "flex";
-    } else {
-        toTopButton.style.display = "none";
-    }
-});
